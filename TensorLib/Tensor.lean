d5e7f16c7f18cd8fa7ca444658942dce8f820a83
Sean McLaughlin <seanmcl@amazon.com>
2024-12-19 16:31:25 -0800

    X2

--- TensorLib/Tensor.lean
+++ TensorLib/Tensor.lean
@@ -162,8 +162,6 @@ def dimIndexToPosition (x : Tensor) (i : DimIndex) : Nat :=
 --! number of bytes representing the entire tensor
 def nbytes (x : Tensor) : ℕ := x.itemsize * x.size
 
-def pr [H : Repr a] (x : a) : String := (H.reprPrec x 100).pretty
-
 /-!
 Copy a Tensor's data to new, contiguous storage.
 
@@ -197,6 +195,10 @@ def copyAndReshape (arr : Tensor) (shape : Shape) : Err Tensor :=
   let arr := arr.copy
   .ok { arr with shape, unitStrides := shape.unitStrides arr.dataOrder }
 
+private def copyAndReshape! (arr : Tensor) (shape : Shape) : Tensor :=
+  match copyAndReshape arr shape with
+  | .error msg => panic! msg
+  | .ok v => v
 
 class Element (a : Type) where
   dtype : Dtype
@@ -248,6 +250,15 @@ def setDimIndex [Element a] (x : Tensor) (index : DimIndex) (v : a): Err Tensor 
   if posn < 0 then .error s!"Illegal position: {posn}"
   else setPosition x posn.toNat v
 
+-- TODO: remove `Err` by proving all indices are within range
+def toList (a : Type) [Tensor.Element a] (x : Tensor) : Err (List a) :=
+  let traverseFn ind : Err a := getDimIndex x ind
+  traverse traverseFn x.shape.allDimIndices
+
+def toList! (a : Type) [Tensor.Element a] (x : Tensor) : List a := match toList a x with
+| .error _ => []
+| .ok x => x
+
 instance BV8Native : Element BV8 where
   dtype := Dtype.mk .uint8 .oneByte
   itemsize := 1
@@ -277,14 +288,85 @@ instance BV64Little : Element BV64 where
   fromByteArray arr startIndex := ByteArray.toBV64 arr startIndex .littleEndian
 
 #guard (arange BV16 10).size == 10
+#guard toList! BV16 (Tensor.Element.arange BV16 10) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+end Element
+
+namespace Format
+open Std.Format
+
+-- Useful for small arrays, e.g. to help with printing and such
+-- There are some natural invariants we could check, such as that the
+-- trees in a node all have the same height, but since this is just a
+-- utility structure we'll keep it simple
+private inductive Tree a where
+| root (xs: List a)
+| node (xs: List (Tree a))
+deriving BEq, Repr
+
+private def toTree {a : Type} (x : List a) (strides : Strides) : Err (Tree a) :=
+  if strides.any fun x => x <= 0 then .error "strides need to be positive" else
+  match strides with
+  | [] => if x.length == 1 then .ok (.root x) else .error "empty shape that's not an array scalar"
+  | [1] => .ok (.root x)
+  | [_] => .error "not a unit stride"
+  | stride :: strides => do
+    let chunks := x.toChunks stride.toNat
+    let res <- chunks.traverse (fun x => toTree x strides)
+    return .node res
+
+private def toTree! {a : Type} (x : List a) (strides : Strides) : Tree a := match toTree x strides with
+| .error _ => .root []
+| .ok t => t
+
+#guard toTree! (Element.toList! BV16 (Element.arange BV16 10)) [5, 1] == .node [.root [0, 1, 2, 3, 4], .root [5, 6, 7, 8, 9]]
+
+private def formatRoot [Repr a] (xs : List a) : Std.Format := sbracket (joinSep (List.map repr xs) (text ", "))
+
+private def formatTree1 [Repr a] (shape : Shape) (t : Tree a) : Err Std.Format := match shape, t with
+| [], .root [x] => .ok $ repr x
+| [n], .root r => if r.length != n then .error "shape mismatch" else .ok (formatRoot r)
+| n :: shape, .node ts => do
+  let fmts <- ts.traverse (formatTree1 shape)
+  if fmts.length != n then .error "head mismatch" else
+  let indented := join (fmts.intersperse (", " ++ line))
+  .ok (group (nest 2 ("[" ++ indented ++ "]")))
+| _, _ => .error "format mismatch"
+
+/- This needs some improvement. For example, I'm not able to get the indent to stick
+at the end of the "array("
+
+$ bin/tensorlib format 20 2
+Got shape [20, 2]
+array([[0x0000#16, 0x0001#16],
+  [0x0002#16, 0x0003#16],
+  [0x0004#16, 0x0005#16],
+  ...
+-/
+private def formatTree [Repr a] (t : Tree a) (shape : Shape) : Err Std.Format := do
+  let r <- formatTree1 shape t
+  return join ["array(", r, ")"]
+
+end Format
+
+def format (a : Type) [Repr a] [Element a] (x : Tensor) : Err Std.Format := do
+  let xs <- Element.toList a x
+  let t <- Format.toTree xs x.unitStrides
+  let f <- Format.formatTree t x.shape
+  return f
+
+def str (a : Type) [Repr a] [Tensor.Element a] (x : Tensor) : String := match format a x with
+| .error err => s!"Error: {err}"
+| .ok s => Std.Format.pretty s 120
+
+#guard str BV8 (Element.arrayScalar (5 : BV8)) == "array(0x05#8)"
+#guard str BV8 (Element.arange BV8 10) == "array([0x00#8, 0x01#8, 0x02#8, 0x03#8, 0x04#8, 0x05#8, 0x06#8, 0x07#8, 0x08#8, 0x09#8])"
 
 section Test
 
-private def x := arange BV8 6
-#eval x.copy
+private def arr0 := Element.arange BV8 10
+#eval (arr0.copyAndReshape! [2, 5]).str BV8
 
 end Test
-
-end Element
 end Tensor
 end TensorLib
