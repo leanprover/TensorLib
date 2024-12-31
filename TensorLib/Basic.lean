142bb8e9f910cf3b982437cb898f345aa5b93c91
Sean McLaughlin <seanmcl@amazon.com>
2024-12-31 10:33:30 -0800

    Remove everything
    This is the first step in copying the sm/format branch over piece
    by piece.

--- TensorLib/Basic.lean
+++
@@ -1,8 +0,0 @@
-/-
-Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Jean-Baptiste Tristan, Paul Govereau, Sean McLaughlin
--/
-
-import TensorLib.NumpyRepr
-import TensorLib.Parse
