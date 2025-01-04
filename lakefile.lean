296fce251e7e82cb85a612a6672c90527eb6c74c
Sean McLaughlin <seanmcl@amazon.com>
2025-01-03 16:34:46 -0800

    Segfault

--- lakefile.lean
+++ lakefile.lean
@@ -11,14 +11,14 @@ lean_lib «TensorLib» where
 lean_exe "tensorlib" where
   root := `Main
 
--- require aesop from git
---   "https://github.com/leanprover-community/aesop" @ "v4.15.0-rc1"
-
--- require Cli from git
---   "https://github.com/seanmcl/lean4-cli.git" @ "v4.15.0-rc1"
-
 require aesop from git
-  "https://github.com/leanprover-community/aesop" @ "v4.14.0"
+  "https://github.com/leanprover-community/aesop" @ "v4.15.0-rc1"
 
 require Cli from git
-  "https://github.com/leanprover/lean4-cli.git" @ "v2.2.0-lv4.14.0-rc1"
+  "https://github.com/seanmcl/lean4-cli.git" @ "v4.15.0-rc1"
+
+-- require aesop from git
+--   "https://github.com/leanprover-community/aesop" @ "v4.14.0"
+
+-- require Cli from git
+--   "https://github.com/leanprover/lean4-cli.git" @ "v2.2.0-lv4.14.0-rc1"
